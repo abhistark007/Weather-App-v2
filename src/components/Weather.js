54b05b37808a7224534f72da7c28680c3c9daf14
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-21 15:01:22 +0530

    Update Weather.js
    Now able to fetch weather data for searched cityname

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,21 +1,44 @@
 import React, { useState } from 'react'
 import WeatherForm from './WeatherForm'
+import './Weather.css';
 
 function Weather() {
-    const [cityWeather,setCityWeather]=useState(null);
-    const [cityController,setCityController]=useState("");
+    const [cityWeather, setCityWeather] = useState(null);
+    const [cityController, setCityController] = useState("");
+    const [loading, setLoading] = useState(false);
 
 
 
-    const getCityData=()=>{
+    const getCityData = () => {
+        setLoading(true);
+        try {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityController}&units=metric&appid=0fd12e44a925b9275b943a063058d142`)
+                .then((res) => res.json())
+                .then((result) => {
+                    console.log(result);
+                    setCityWeather(result)
+                });
 
+        } catch (e) {
+            console.log("Error occured inside getCityData() function");
+        }
+        setLoading(false);
     }
 
-  return (
-    <div className='flex flex-col bg-black opacity-90 mt-16 mx-auto w-[70vw] min-h-[90vh] rounded-xl'>
-        <WeatherForm cityController={cityController} setCityController={setCityController}/>
-    </div>
-  )
+    return (
+        <div className='flex flex-col bg-black opacity-90 mt-16 mx-auto w-[70vw] min-h-[90vh] rounded-xl'>
+            <WeatherForm cityController={cityController} setCityController={setCityController} getCityData={getCityData} />
+            {
+                loading ? (<div className='w-full mx-10 mt-20'>
+                    <div class="custom-loader"></div>
+                </div>) :
+                    (cityWeather === null ? (<></>) :
+                        (<div className='text-white'>
+                            <li>{cityWeather.main.temp}</li>
+                        </div>))
+            }
+        </div>
+    )
 }
 
 export default Weather
