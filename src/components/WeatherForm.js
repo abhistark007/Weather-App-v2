bebf9993096ec666d641122b5d9d37a3a69cb343
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-21 15:00:55 +0530

    Update WeatherForm.js

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import {FaSearchengin} from 'react-icons/fa'
 
-function WeatherForm({cityController,setCityController}) {
+function WeatherForm({cityController,setCityController,getCityData}) {
   return (
     <div className='flex bg-white mx-10 justify-between rounded-3xl my-5'>
-        <input  className=' rounded-l-3xl h-12 outline-none px-4 flex-1 ' placeholder='City Name' value={cityController}
+        <input  className=' rounded-l-3xl h-12 outline-none px-4 flex-1 max-sm:w-10' placeholder='City Name' value={cityController}
         onChange={(e)=>setCityController(e.target.value)}
         />
-        <div className='flex text-white text-3xl bg-purple-600 rounded-r-3xl justify-between items-center px-4 duration-200 hover:scale-105 cursor-pointer'>
+        <div 
+        onClick={getCityData}
+        className='flex text-white text-3xl bg-purple-600 rounded-r-3xl justify-between items-center px-4 duration-200 hover:scale-105 cursor-pointer'>
             <FaSearchengin/>
         </div>
     </div>
