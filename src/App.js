94020a3810b08735989984861764aed7bb3e1d70
abhistark007 <abhi.kumar2019@vitstudent.ac.in>
2023-05-21 14:31:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import myMusic from './assets/bgMusic.mp3'
 import bgVideo from './assets/bgVideo2.mp4'
 import './App.css';
+import Weather from './components/Weather';
 
 function App() {
   
 
   return (
-    <div className="App min-h-screen relative bg-black">
+    <div className="App min-h-screen relative bg-black flex flex-col">
       <div className="absolute top-0 left-0 z-10">
           <audio controls loop >
             <source src={myMusic} type="audio/mp3"/>
@@ -16,6 +17,8 @@ function App() {
       <video autoPlay muted loop  className="absolute w-full max-h-screen object-cover left-0 right-0 top-0 opacity-50">
           <source src={bgVideo} type="video/mp4"/>
       </video>
+
+      <Weather/>
 
 
     </div>
